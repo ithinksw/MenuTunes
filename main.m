554646115f02bc4f6749b4654f3e1b410ac46a40
Matthew Judy <mjudy@ithinksw.com>
2003-03-30 18:17:35 +0000

    Testing Matt's cvs

--- main.m
+++ main.m
@@ -5,7 +5,7 @@
 //  Created by Kent Sutherland on Sun Nov 17 2002.
 //  Copyright (c) 2002 Kent Sutherland. All rights reserved.
 //
-
+// Poink.
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char *argv[])
